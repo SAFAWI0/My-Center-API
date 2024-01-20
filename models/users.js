313f54a9df13e025dd1166642d71b4a83e65df28
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-01-20 15:49:18 +0300

    update

--- models/users.js
+++ models/users.js
@@ -51,7 +51,27 @@ async function login(req, res) {
   }
 }
 
+
+
+
+async function filter(req, res) {
+  try{
+    let { email ,name} = req.body;
+    const result = await client.query(`SELECT * FROM users WHERE email = '${email}'OR name='${name}'`);
+    res.send(result.rows);
+  }catch{
+
+
+  }
+}
+
+
+
+
+
+
 module.exports = {
   regster,
   login,
+  filter,
 };
