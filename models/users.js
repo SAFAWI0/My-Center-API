4b7cefdc3a0f9d83596806694d1b11f25dbf0c2e
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-01-26 13:47:34 +0300

    update

--- models/users.js
+++ models/users.js
@@ -1,5 +1,5 @@
 const client = require("../db/index");
-//const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
 
 async function alluser(req, res) {
@@ -15,10 +15,10 @@ async function alluser(req, res) {
 async function regster(req, res) {
   try {
     let { name, email, password, phone } = req.body;
-   // const hashPasswod = bcrypt.hashSync(password, 10);
+    const hashPasswod = bcrypt.hashSync(password, 10);
     const result =
       await client.query(`insert into users(name,email,password,phone)
-      values('${name}','${email}','${password}','${phone}') RETURNING *`);
+      values('${name}','${email}','${hashPasswod}','${phone}') RETURNING *`);
     res.send({
       success: true,
       user: result.rows[0],
@@ -50,8 +50,7 @@ async function login(req, res) {
       res.send({ success: false, msg: "User not found" });
     else {
       let user = result.rows[0];
-     // const match = await bcrypt.compare(password, user.password);
-      const match = await password;
+     const match = await bcrypt.compare(password, user.password);
       if (match) {
         var tokenuser = jwt.sign(user, "users");
         res.send({ success: true, tokenuser, user });
@@ -75,9 +74,9 @@ async function filter(req, res) {
 async function updateUsers(req, res) {
   let id = req.params.id;
   let { name, phone, email, password } = req.body;
- // const hashPasswod = bcrypt.hashSync(password, 10);
+  const hashPasswod = bcrypt.hashSync(password, 10);
   const result = await client.query(`UPDATE users
-  SET name = '${name}' , phone = '${phone}' , email = '${email}',password='${password}' WHERE user_id = ${id} RETURNING *`);
+  SET name = '${name}' , phone = '${phone}' , email = '${email}',password='${hashPasswod}' WHERE user_id = ${id} RETURNING *`);
   res.send(result.rows);
 }
 
